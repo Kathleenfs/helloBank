7372806d034765ab924998d250c1ba3db0d86b1b
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-15 17:49:42 -0300

    criação DAO e service Transacao

--- src/main/java/com/projeto/helloBank/daos/ContaDAO.java
+++ src/main/java/com/projeto/helloBank/daos/ContaDAO.java
@@ -2,6 +2,7 @@ package com.projeto.helloBank.daos;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 import com.projeto.helloBank.models.Conta;
 
 public interface ContaDAO extends CrudRepository<Conta, Integer> {
